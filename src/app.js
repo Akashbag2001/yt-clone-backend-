a4707863cf3fe404b71d4efec94f259f6898bc29
Akashbag2001 <bagakash11@gmail.com>
2024-08-07 16:54:20 +0530

    Util code updates

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from 'cors'
+const app = express();
+const limit = "16kb"
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    Credential: true,
+}))
+
+app.use(express.json({
+    limit: limit,
+}))
+app.use(express.urlencoded({
+    extended: true,
+    limit: limit
+}))
+app.use(express.static('public'));
+app.use(cookieParser());
+
+
+export {app} 
--- src/index.js
+++ src/index.js
@@ -6,8 +6,20 @@ import connectDB from './db/index.js';
 dotenv.config({
     path: './env'
 })
-
+const PORT = process.env.PORT || 8000
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("ERROR: ", error);
+        throw error
+    })
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on ${PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("MONGODB connection Error!!" + error);
+})
 
 
 
@@ -22,10 +34,10 @@ connectDB()
 // ;(async()=>{
 //     try {
 //         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         app.on("error",(error)=>{
-//             console.log("ERROR: ", error);
-//             throw error
-//         })
+        // app.on("error",(error)=>{
+        //     console.log("ERROR: ", error);
+        //     throw error
+        // })
 
 //         app.listen(process.env.PORT, ()=>{
 //             console.log(`App is listening on port: ${process.env.PORT}`)
