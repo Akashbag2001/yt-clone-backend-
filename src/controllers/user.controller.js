45da8ee550cb8e06c4c94e6f05bf67baee50a29c
Akashbag2001 <bagakash11@gmail.com>
2024-09-10 10:53:40 +0530

    Add register controller and big fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -54,6 +54,10 @@ const registerUser = asyncHandler(async (req, res) => {
   if (!createdUser) {
     throw new ApiError(500, "Something went wrong while registering user");
   }
+  return res.status(201).json(
+    new ApiResponse(200, createdUser, "User Registered successfully!")
+  )
+
 });
 
 export { registerUser };
